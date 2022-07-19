2f8d92912d2f4767ecd0e57d11db1feb6d066c3f
Michael Yegorov <michael.yegorov@socialedgeconsulting.com>
2022-07-19 11:29:56 +0200

    SE-343: analyzed and approved configuration.xml, configuration.vm.ejs and configuration-helpers.vm for usage with embeddables (testing is still needed)

--- generators/embed/templates/src/shared/utils/verintEnv.js
+++ generators/embed/templates/src/shared/utils/verintEnv.js
@@ -1,14 +1,17 @@
-/* eslint no-param-reassign: 'off' */
+/* eslint-disable no-param-reassign,import/no-unresolved,import/prefer-default-export */
 import jQuery from 'verint/jquery'
 
 export function initializeWidget (widgetName, widgetPart, registerCallback) {
+  // eslint-disable-next-line id-match
   (function extendJQuery ($) {
     // ensure widget availability in the global scope:
     if (typeof $.telligent === 'undefined') $.telligent = {}
     // todo: what is evolution part? is it necessary?
     if (typeof $.telligent.evolution === 'undefined') $.telligent.evolution = {}
     if (typeof $.telligent.evolution.widgets === 'undefined') $.telligent.evolution.widgets = {}
-    if (typeof $.telligent.evolution.widgets[widgetName] === 'undefined') $.telligent.evolution.widgets[widgetName] = {}
+    if (typeof $.telligent.evolution.widgets[widgetName] === 'undefined') {
+      $.telligent.evolution.widgets[widgetName] = {}
+    }
     $.telligent.evolution.widgets[widgetName][widgetPart] = {
       register: registerCallback,
     }
