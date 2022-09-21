6a5382151c547b946bc0e7ba027bb6363f08d154
Michael Yegorov <michael.yegorov@socialedgeconsulting.com>
2022-09-21 14:58:36 -0700

    INTERNAL-890: creation of article theme

--- src/constants/global.js
+++ src/constants/global.js
@@ -1,14 +1,16 @@
 // eslint-disable-next-line import/no-unused-modules
 const themeTypeIds = {
-  site:  '0c647246673542f9875dc8b991fe739b',
-  group: 'c6108064af6511ddb074de1a56d89593',
-  blog:  'a3b17ab0af5f11dda3501fcf55d89593',
+  site:    '0c647246673542f9875dc8b991fe739b',
+  group:   'c6108064af6511ddb074de1a56d89593',
+  blog:    'a3b17ab0af5f11dda3501fcf55d89593',
+  article: '81d59b9a7fa44a7b98ee7e3c91067510',
 }
 
 const themeTypeFolders = {
-  site:  's',
-  group: 'g',
-  blog:  'b',
+  site:    's',
+  group:   'g',
+  blog:    'b',
+  article: 'a', // todo: check if it's correct letter (need to look on)
 }
 
 const themeStaticFiles = {
